5fd817ec4c3287e62475f93ab141becc7b221432
Sergei Konstantinov <konstantinov-sa@bk.ru>
2022-06-14 09:23:22 +0300

    add CreateMessage func

--- tg/tg.go
+++ tg/tg.go
@@ -1,6 +1,7 @@
 package tg
 
 import (
+	"wallet_tgbot/command"
 	"wallet_tgbot/utils"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -17,4 +18,33 @@ func InitTgBot() (bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel, err err
 	updates = bot.GetUpdatesChan(u)
 
 	return
+}
+
+func CreateMessage(msg command.ChatData) tgbotapi.MessageConfig {
+	m := tgbotapi.NewMessage(msg.ChatId, msg.Msg)
+	m.ParseMode = "markdown"
+
+	var numericKeyboard tgbotapi.InlineKeyboardMarkup
+	if len(msg.Params) > 0 {
+		numericKeyboard = tgbotapi.NewInlineKeyboardMarkup()
+		columnCount := 1
+
+		for i, v := range msg.Params {
+			if i%columnCount == 0 {
+				numericKeyboard.InlineKeyboard = append(
+					numericKeyboard.InlineKeyboard,
+					tgbotapi.NewInlineKeyboardRow(),
+				)
+			}
+
+			numericKeyboard.InlineKeyboard[i/columnCount] = append(
+				numericKeyboard.InlineKeyboard[i/columnCount],
+				tgbotapi.NewInlineKeyboardButtonData(v, v),
+			)
+		}
+	}
+
+	m.ReplyMarkup = numericKeyboard
+
+	return m
 }
